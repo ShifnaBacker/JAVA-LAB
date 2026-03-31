04823c9fde425caee68c823746ce4afa882a2204
ShifnaBacker <shifnanilu@gmail.com>
2026-03-31 15:30:11 +0530

    priority queue

---
+++ queue.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Queue
+{
+public static void main(String[] args)
+{
+PriorityQueue<String>q=new PriorityQueue<String>();
+Scanner sc=new Scanner(System.in);
+String el;
+int ch;
+do
+{
+System.out.print("\n----------------\n1:add\n2:remove\n3:display\n4:head\0:wrong choice\n----------------\n Enter your choice: ");
+
+ch=sc.nextInt();
+switch(ch)
+{
+case 1:
+System.out.print("Enter element to insert:\n");
+el=sc.next();
+import java.util.*;
+public class Queue()
+{
+public static void main(String[] args)
+{
+PriorityQueue<String>q=new PriorityQueue<String>();
+Scanner sc=new Scanner(System.in);
+String el;
+int ch;
+do
+{
+System.out.print("\n----------------\n1:add\n2:remove
+\n3:display\n4:head\0:wrong choice\n-------
+---------\n Enter your choice: ");
+
+ch=sc.nextInt();
+switch(ch)
+{
+case 1:
+System.out.print("Enter element to insert:\n");
+el=sc.next();
